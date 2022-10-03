cf1be5b63b9f2b695386fca65927aedffb2ea682
kondors1995 <normandija1945@gmail.com>
2022-10-03 17:34:41 +0300

    Nuke TG code

--- build-tc.sh
+++ build-tc.sh
@@ -9,40 +9,16 @@ err() {
     echo -e "\e[1;41m$*\e[0m"
 }
 
-# Set Chat ID, to push Notifications
-CHATID="-1001231303646"
-
 # Set a directory
 DIR="$(pwd ...)"
-
-# Inlined function to post a message
-export BOT_MSG_URL="https://api.telegram.org/bot$TOKEN/sendMessage"
-tg_post_msg() {
-	curl -s -X POST "$BOT_MSG_URL" -d chat_id="$CHATID" \
-	-d "disable_web_page_preview=true" \
-	-d "parse_mode=html" \
-	-d text="$1"
-
-}
-tg_post_build() {
-	curl --progress-bar -F document=@"$1" "$BOT_BUILD_URL" \
-	-F chat_id="$2"  \
-	-F "disable_web_page_preview=true" \
-	-F "parse_mode=html" \
-	-F caption="$3"
-}
 
 # Build Info
 rel_date="$(date "+%Y%m%d")" # ISO 8601 format
 rel_friendly_date="$(date "+%B %-d, %Y")" # "Month day, year" format
 builder_commit="$(git rev-parse HEAD)"
 
-# Send a notificaton to TG
-tg_post_msg "<b>Soviet Clang Compilation Started</b>%0A<b>Date : </b><code>$rel_friendly_date</code>%0A<b>Toolchain Script Commit : </b><code>$builder_commit</code>%0A"
-
 # Build LLVM
 msg "Building LLVM..."
-tg_post_msg "<code>Building LLVM</code>"
 ./build-llvm.py \
 	--clang-vendor "Soviet" \
 	--defines LLVM_PARALLEL_COMPILE_JOBS=$(nproc) LLVM_PARALLEL_LINK_JOBS=$(nproc) CMAKE_C_FLAGS=-O3 CMAKE_CXX_FLAGS=-O3 \
@@ -57,13 +33,11 @@ tg_post_msg "<code>Building LLVM</code>"
 # Check if the final clang binary exists or not.
 [ ! -f install/bin/clang-1* ] && {
 	err "Building LLVM failed ! Kindly check errors !!"
-	tg_post_build "build.log" "$CHATID" "Error Log"
 	exit 1
 }
 
 # Build binutils
 msg "Building binutils..."
-tg_post_msg "<code>Building Binutils</code>"
 ./build-binutils.py --targets arm aarch64
 
 # Remove unused products
@@ -94,9 +68,7 @@ llvm_commit_url="https://github.com/llvm/llvm-project/commit/$short_llvm_commit"
 binutils_ver="$(ls | grep "^binutils-" | sed "s/binutils-//g")"
 clang_version="$(install/bin/clang --version | head -n1 | cut -d' ' -f4)"
 
-tg_post_msg "<b>Soviet clang compilation Finished</b>%0A<b>Clang Version : </b><code>$clang_version</code>%0A<b>LLVM Commit : </b><code>$llvm_commit_url</code>%0A<b>Binutils Version : </b><code>$binutils_ver</code>"
-
-# Push to GitHub
+# Push to GitLab
 # Update Git repository
 git config --global user.name "kondors1995"
 git config --global user.email "normandija1945@gmail.com"
@@ -123,5 +95,3 @@ popd || exit
 
 # Set git buffer to original size
 git config --global http.version HTTP/2
-
-tg_post_msg "<b>Toolchain Compilation Finished and pushed</b>"
